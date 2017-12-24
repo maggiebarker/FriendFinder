c7da23a12c0cdf0dc97e7bda7917b2c7abdaa2ae
maggiebarker <maggieb.06@gmail.com>
2017-12-23 20:53:33 -0700

    updated everything

---
+++ app/data/friends.js
@@ -0,0 +1,48 @@
+//create array of default friends
+
+var friends = [{
+    name: "Tony Stark",
+    photo: "https://vignette.wikia.nocookie.net/moviedatabase/images/8/8c/Tony_Stark.jpg/revision/latest/scale-to-width-down/250?cb=20150430161420",
+    scores: [5, 2, 1, 5, 3, 4, 5, 5, 3, 4]
+  }, {
+    name: "Diana Prince",
+    photo: "http://www.forevernews.in/wp-content/uploads/2017/06/IMG-20170607-WA0006.jpg",
+    scores: [2, 5, 4, 3, 2, 1, 5, 3, 5, 5]
+  }, {
+    name: "Bruce Banner",
+    photo: "https://i.pinimg.com/736x/02/8a/95/028a95198831d378a523eed51387e364--marvel-avengers-marvel-comics.jpg",
+    scores: [4, 2, 1, 1, 5, 2, 5, 2, 2, 3]
+  },{
+    name: "Cersei Lannister",
+    photo: "https://timedotcom.files.wordpress.com/2017/06/cersei-lannister-lena-headey-costume-3.jpg?quality=85&w=372",
+    scores: [5, 5, 5, 5, 5, 5, 1, 5, 1, 5]
+  }, {
+    name: "James Kirk",
+    photo: "https://vignette.wikia.nocookie.net/memoryalpha/images/c/c5/James_T._Kirk%2C_alternate_reality.jpg/revision/latest/scale-to-width-down/350?cb=20160813174006&path-prefix=en",
+    scores: [3, 5, 5, 5, 1, 2, 3, 4, 5, 4]
+  }, {
+    name: "Sookie Stackhouse",
+    photo: "https://i.pinimg.com/originals/b5/88/d8/b588d85d5d94751e65046b8d985553fd.jpg",
+    scores: [1, 3, 2, 1, 3, 1, 2, 3, 3, 5]
+  }, {
+    name: "Arya Stark",
+    photo: "https://vignette.wikia.nocookie.net/gameofthrones/images/9/9f/704_Arya_Profil.jpg/revision/latest/scale-to-width-down/310?cb=20170808162606&path-prefix=de",
+    scores: [3, 5, 5, 3, 3, 3, 3, 3, 3, 4]
+  },{
+    name: "Charles Xavier",
+    photo: "http://images5.fanpop.com/image/photos/28900000/Professor-Charles-Xavier-professor-charles-francis-xavier-28939850-300-350.jpg",
+    scores: [4, 4, 4, 3, 1, 1, 3, 3, 5, 3]
+  },{
+    name: "Hermione Granger",
+    photo: "https://vignette.wikia.nocookie.net/harrypotterfanon/images/0/0b/Hermione_Granger_%28Ravenclaw%29_II.jpg/revision/latest/scale-to-width-down/350?cb=20160314065431",
+    scores: [4, 4, 1, 3, 4, 3, 3, 4, 5, 2]
+  },{
+    name: "Dean Winchester",
+    photo: "https://i.pinimg.com/736x/cb/b2/27/cbb2278bd7d09ca7fb8152e8e92b36f5--perfect-smile-beautiful-smile.jpgg",
+    scores: [2, 5, 4, 5, 3, 4, 5, 5, 5, 5]
+  }];
+  
+   //export array
+  
+  module.exports = friends;
+  
--- server.js
+++ server.js
@@ -1,25 +1,28 @@
-//Dependencies
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+// ===================================Dependencies========================================
 
-// Configure Express
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// ==============================Set up the Express App========================================
+
 var app = express();
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 8000;
 
-// Expose the public directory to access CSS files
-app.use(express.static(path.join(__dirname, './app/public')));
+// Sets up the Express app to handle data parsing
 
-// Add middleware for parsing incoming request bodies
+app.use(express.static(__dirname + "/app/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Add the application routes
-require(path.join(__dirname, './app/routing/apiRoutes'))(app);
-require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+// ==============================Require routing========================================
 
-// Start listening on PORT
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// =========================Starts the server to begin listening================================
+
 app.listen(PORT, function() {
-  console.log('Friend Finder app is listening on PORT: ' + PORT);
+  console.log("App listening on PORT " + PORT);
 });
